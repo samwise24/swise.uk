e11f867819fa3680ecf8a1f11365061b31cdbc4f
Sam Wise <swise@socratesint.com>
2017-11-22 15:41:32 +0000

    adding in bits and bobs now

--- index.php
+++ index.php
@@ -4,16 +4,15 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-
-
 <html>
     <head>
+        <?php include('head.html'); ?>
         <title>Sam Wise</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <div>hello world</div>
-        
+        <p> hello world</p>
+        <?php
+        // put your code here
+        ?>
     </body>
 </html>
