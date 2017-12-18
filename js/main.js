b00301824fe4b91b05470d19ae77be2c24ca8d16
Sam Wise <swise@socratesint.com>
2017-12-18 14:26:36 +0000

    styling changes, Js attempt, not finsihed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+jQuery(document).ready(function(){
+    $(function(){
+        var shrinkHeader = 300;
+         $(window).scroll(function() {
+           var scroll = getCurrentScroll();
+             if ( scroll >= shrinkHeader ) {
+                  $('#menu-divider,.hero-wrapper').addClass('shrink');
+               }
+               else {
+                   $('#menu-divider, .hero-wrapper').removeClass('shrink');
+               }
+         });
+       function getCurrentScroll() {
+           return window.pageYOffset || document.documentElement.scrollTop;
+           }
+    });
+});
